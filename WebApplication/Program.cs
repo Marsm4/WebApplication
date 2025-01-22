f3d5efcf83d0ce6c699bab861474c71563116837
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2025-01-22 20:55:51 +0300

    апи готова . но при миграции созатеся только системная таблица

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,29 +1,25 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using MyWebApp.Data;
 using MyWebApp.Services;
 using MyWebApp.Interfaces;
-
 using Microsoft.AspNetCore.Builder;
-using static MyWebApp.Data.ContextDb;
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем сервисы
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Добавляем DbContext
+// Настройка строки подключения
 builder.Services.AddDbContext<ContextDb>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 // Регистрируем сервисы
 builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -33,11 +29,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
 
 app.Run();
